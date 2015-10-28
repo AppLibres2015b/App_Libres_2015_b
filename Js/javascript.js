a387984c710217c4d4fed467fed44257faf92c33
Adrian Eguez <eadepto@hotmail.com>
2015-10-28 08:49:29 -0500

    Objetos javascript

--- Js/javascript.js
+++ Js/javascript.js
@@ -1,5 +1,5 @@
 
-var number = 1;
+//var number = 1;
 //Comentario de Una linea
 /*
 while(number<=5){
@@ -9,43 +9,60 @@ while(number<=5){
 */
 
 
-for(var numero=1; numero<=5;numero++){
-    if(numero%2==0){
-        
-        if(numero==2||numero==4){
-            console.log("Yei es 2 o 4");
-        }
-        
-        console.log("El numero "+numero+" es par");
-        
-        
+//for(var numero=1; numero<=5;numero++){
+//    if(numero%2==0){
+//        
+//        if(numero==2||numero==4){
+//            console.log("Yei es 2 o 4");
+//        }
+//        
+//        console.log("El numero "+numero+" es par");
+//        
+//        
+//    }
+//    else{
+//        console.log("El numero "+numero+" es impar");
+//    }
+//    
+//    
+//    
+//}
+//
+//
+//function sumaCubos (a,b){
+//    return (a*a*a)+(b*b*b);
+//}
+//
+//var suma = sumaCubos(4,2);
+//
+//console.log(suma);
+
+var amigos = ["Zeus","Thor","Isis","Gaia","Jesus"];
+
+//console.log(amigos);
+
+var datos = {
+    nombre:"Adrian",
+    apellido:"Eguez",
+    desplegarNombre: function(){
+        console.log(datos.nombre+" "+this.apellido);
     }
-    else{
-        console.log("El numero "+numero+" es impar");
-    }
-    
-    
-    
 }
 
-
-function sumaCubos (a,b){
-    return (a*a*a)+(b*b*b);
-}
-
-var suma = sumaCubos(4,2);
-
-console.log(suma);
+console.log(datos.nombre);
+console.log(datos.apellido);
+console.log(datos.desplegarNombre());
 
 
 
+console.log(typeof amigos);
+
+console.log(amigos);
+
+console.log(typeof datos);
 
 
-
-
-
-
-
+console.log(datos);
 
 
 
