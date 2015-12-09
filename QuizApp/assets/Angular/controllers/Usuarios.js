4f0a0a9018f54c1ea8f66abea7178accd38ffdeb
Adrian Eguez <eadepto@hotmail.com>
2015-12-09 09:01:45 -0500

    Agregar Funcion Agregar usuario

--- QuizApp/assets/Angular/controllers/Usuarios.js
+++ QuizApp/assets/Angular/controllers/Usuarios.js
@@ -5,13 +5,24 @@ app.controller('UsuariosController', ['$scope',"$http",function($scope,$http){
 //$scope.agregarUsuario = function agregarUsuario(){
 //
 //}
-   
+    
+//$scope.nombre = "";
+//$scope.apellido= "";
+//$scope.password= "";
+//$scope.correo= "";
+//$scope.fecha= "";
+
+    
 $scope.agregarUsuario = agregarUsuario;
+
     
+function agregarUsuario(datos){
     
-    
-function agregarUsuario(){
-    
+    console.log("Entro a agregar Usuario",$scope.nombre);
+    console.log("Appelido",$scope.apellido);
+    console.log("password",$scope.password);
+    console.log("correo",$scope.correo);
+    console.log("fecha",$scope.fecha);
     
 
 }
