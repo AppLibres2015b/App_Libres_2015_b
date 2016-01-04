252b4580d5c1a58fceac92e1cf6db5c51e1a903b
Adrian Eguez <eadepto@hotmail.com>
2016-01-04 07:28:31 -0500

    Arreglar guardado

--- QuizApp/assets/Angular/controllers/EditarUsuarios.js
+++ QuizApp/assets/Angular/controllers/EditarUsuarios.js
@@ -1,62 +1,58 @@
-app.controller('EditarUsuarioController', ['$scope', "$http",'$stateParams','PruebasFactory', function ($scope, $http, $stateParams, PruebasFactory) {
+app.controller('EditarUsuarioController', ['$scope', "$http", '$stateParams', 'PruebasFactory', function ($scope, $http, $stateParams, PruebasFactory) {
 
     console.log("Entro a Editar usuario");
     console.log($stateParams.idUsuario);
 
     $http({
         method: 'GET',
-        url: 'http://localhost:1337/Usuarios/'+$stateParams.idUsuario
+        url: 'http://localhost:1337/Usuarios/' + $stateParams.idUsuario
     }).then(
         function exitoEnElGuardado(respuesta) {
             console.log(respuesta);
             $scope.usuario = respuesta.data;
+            console.log($scope.usuario);
         },
         function falloEnElGuardado(error) {
             console.log(error);
         });
 
-   
-    
+
+
     PruebasFactory.busqueda({
-        idUsuario:$stateParams.idUsuario
-    })
-    .$promise.then(
-        function llegaron(respuesta)
-        {
-            console.log(respuesta);
-            $scope.pruebas = respuesta;
-        },
-        function Errores(error)
-        {
-            console.log(error);
-        });
+            idUsuario: $stateParams.idUsuario
+        })
+        .$promise.then(
+            function llegaron(respuesta) {
+                console.log(respuesta);
+                $scope.pruebas = respuesta;
+            },
+            function Errores(error) {
+                console.log(error);
+            });
     
+    $scope.guardarPrueba = guardarPrueba;
     
-    PruebasFactory.save({
-        nombre: $scope.nuevaPrueba.nombre
-    })
-    .$promise.then(
-        function llegaron(respuesta)
-        {
-            console.log(respuesta);
-            $scope.pruebas.push(respuesta);
-        },
-        function Errores(error)
-        {
-            console.log(error);
-        });
-    
-    
-    
+
+    function guardarPrueba() {
+
+        PruebasFactory.save({
+                nombre: $scope.nuevaPrueba.nombre,
+                idUsuario: $stateParams.idUsuario
+            })
+            .$promise.then(
+                function llegaron(respuesta) {
+                    console.log(respuesta);
+                    $scope.pruebas.push(respuesta);
+                    $scope.nuevaPrueba.nombre = '';
+                },
+                function Errores(error) {
+                    console.log(error);
+                });
+
+
+    }
+
+
+
+
 }]);
-
-
-
-
-
-
-
-
-
-
-
